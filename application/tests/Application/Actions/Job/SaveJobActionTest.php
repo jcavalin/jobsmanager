65291f216d0d7f2503e6b181e3b7d52954e0b602
Julio Cavalin <julio.cavalin@gmail.com>
2022-03-20 19:13:41 -0300

    feat: Add unit tests

--- application/tests/Application/Actions/Job/SaveJobActionTest.php
+++ application/tests/Application/Actions/Job/SaveJobActionTest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Tests\Application\Actions\Job;
 
+use App\Domain\Job\JobManagerNotifier;
 use App\Domain\Job\JobRepository;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
 use Tests\TestCase;
 
 class SaveJobActionTest extends TestCase
@@ -27,5 +30,28 @@ class SaveJobActionTest extends TestCase
         $jobData       = $jobRepository->findById($payload['data']['id']);
 
         $this->assertNotEmpty($jobData);
+    }
+
+    public function testShouldNotifyManager()
+    {
+        $app = $this->getAppInstance();
+
+        $notified            = false;
+        $jobNotifierProphecy = $this->prophesize(JobManagerNotifier::class);
+        /** @var ObjectProphecy */
+        $jobNotifierProphecy->notify(Argument::any())->will(function () use (&$notified) {
+            $notified = true;
+        });
+        $app->getContainer()->set(JobManagerNotifier::class, $jobNotifierProphecy->reveal());
+
+        $request = $this->createRequest('POST', '/jobs')
+            ->withParsedBody([
+                'user'        => 'regular.1@jobsmapp.com',
+                'title'       => 'Job Title',
+                'description' => 'Job Description',
+            ]);
+        $app->handle($request);
+
+        $this->assertTrue($notified);
     }
 }
